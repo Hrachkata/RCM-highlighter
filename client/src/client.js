77bc66ae31de964ec73ab1dc76e78f6e4b029384
Angel Genev <angel.genev@volley-labs.com>
2025-01-10 12:43:09 +0200

    Addeed C# server, JS client, broke a few things

---
+++ client/src/client.js
@@ -0,0 +1,41 @@
+const vscode = require('vscode');
+const path = require('path');
+
+const { LanguageClient, LanguageClientOptions, ServerOptions } = require('vscode-languageclient/node');
+
+// Defines the search path of your language server DLL. (.NET Core)
+const languageServerPath = "server/bin/Debug/net8.0/RcmServer.dll";
+const languageServerDll = "RcmServer.dll";
+
+function activate(context) {
+    let workPath = path.dirname(context.asAbsolutePath(languageServerPath));
+
+    let serverOptions = {
+        run: { command: "dotnet", args: [languageServerDll], options: { cwd: workPath } },
+        debug: { command: "dotnet", args: [languageServerDll, "--debug"], options: { cwd: workPath } }
+    };
+    
+
+    let clientOptions = {
+        documentSelector: [{ scheme: 'file', language: 'xml' }],
+        synchronize: {
+            fileEvents: vscode.workspace.createFileSystemWatcher('**/.clientrc')
+        }
+    };
+
+    let client = new LanguageClient(
+        'xmljs-rcm-highlight',  // Unique id for your client
+        'XML JS RCM Language Client',
+        serverOptions,
+        clientOptions
+    );
+
+    client.start();
+
+    context.subscriptions.push(client);
+}
+
+module.exports = {
+    activate,
+    deactivate: () => client ? client.stop() : undefined
+};
